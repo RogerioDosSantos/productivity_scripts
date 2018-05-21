72ee857635675aada7323f13e6a8856cfc9537ab
Roger <roger.santos.main@gmail.com>
2018-05-21 06:24:42 -0500

    Improved template to add log

--- bash_template.sh
+++ bash_template.sh
@@ -1,72 +1,99 @@
 #!/bin/bash
 
-# # Ensure that run as root
-# if [ "$EUID" -ne 0 ]
-#   then echo "This program must be run with administrator privileges.  Aborting"
-#   exit
-# fi
-
 # Exit on any non-zero status.
 trap 'exit' ERR
 set -E
 
 # Functions
 
+Log()
+{
+  # Log <type> <level> <message> <detail>
+
+  if [ "${config_log_enabled}" != "1" ]; then
+    return 0
+  fi
+
+  local log_type=$1
+  local log_level=$2
+  local log_message=$3
+  local log_detail=$4
+  local log_date="$(date '+%Y-%m-%d %H:%M:%S')"
+  local log_caller=${FUNCNAME[1]}
+  echo "${log_date},${log_type},${log_level},${log_caller},${log_message},${log_detail}"
+}
+
 Init()
 {
   # Setup - Go to the directory where the bash file is
-  call_dir=$(pwd)
+  g_script_name="$(basename "$0")"
+  g_caller_dir=$(pwd)
   cd "$(dirname "$0")"
-  bash_dir=$(pwd)
-  echo "- Called from ${call_dir}"
-  echo "- Running from ${bash_dir}"
+  g_script_dir=$(pwd)
   cd "$(dirname "$0")"
 }
 
 End()
 {
   # Setup - Return to the called directory
-  cd "${call_dir}"
+  cd "${g_caller_dir}"
 }
 
-DisplayHelpAndExit()
+ScriptDetail()
 {
-	echo "Commands Help"
-  echo "--help (-h) : Display this command help"
-  echo "--xxx (-x) <name>: Config with parameter (Default: ${config_xxx})"
-  echo "--yyy (-y) : Config without parameter"
-  exit 1
+  Log "info" "5" "Script Name" "${g_script_name}"
+  Log "info" "5" "Caller Directory" "${g_caller_dir}"
+  Log "info" "5" "Script Directory" "${g_script_dir}"
+}
+
+DisplayHelp()
+{
+  echo " "
+  echo "${g_script_name} Help"
+  echo " "
+  echo "${g_script_name} <command> [<command_options>]"
+  echo " "
+  echo "- Commands:"
+  echo "--help (h) : Display this command help"
+  echo "--log_enable (-le) : Enable log"
+  echo "--log_level (-ll) <level>: Define the Log Level (Default: ${config_log_level})"
+  echo "--log_type (-lt) <type>: Define the Log Type [Options: all, error, warning, info] (Default: ${config_log_type})"
+  echo " "
 }
 
 GetConfiguration()
 {
-  echo "* $(basename "$0")"
-  echo "- Configuration:"
-  config_xxx="default_value_x"
-  config_yyy="0"
+  config_log_enabled="0"
+  config_log_level="1"
+  config_log_type="all"
   while [[ $# != 0 ]]; do
       case $1 in
-          --xxx|-x)
-            config_xxx="$2"
-            echo "  xxx = ${config_xxx}"
+          --log_enable|-le)
+            config_log_enabled="1"
+            Log "info" "1" "Log Enabled" ""
+            shift 1
+            ;;
+          --log_level|-le)
+            config_log_level="$2"
+            Log "info" "1" "Log Level" "${config_log_level}"
             shift 2
             ;;
-          --yyy|-y)
-            config_yyy="1"
-            echo "  yyy = ${config_yyy}"
-            shift 1
+          --log_type|-lt)
+            config_log_type="$2"
+            Log "info" "1" "Log Type" "${config_log_type}"
+            shift 2
             ;;
           --)
               shift
               break
               ;;
           --help|-h)
-              DisplayHelpAndExit
+              DisplayHelp
               exit
               ;;
           -*)
-              echo "Unknown option $1"
-              DisplayHelpAndExit
+              Log "error" "1" "Unknown option" "$1"
+              DisplayHelp
               exit
               ;;
           *)
@@ -78,11 +105,13 @@ GetConfiguration()
 
 MainFunction()
 {
-  echo "- TODO: Add logic here"
+  Log "info" "1" "TODO" "Add logic here"
 }
 
+# Main
 Init
 GetConfiguration "$@"
+ScriptDetail
 MainFunction
 End
 
