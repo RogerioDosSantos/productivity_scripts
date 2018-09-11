1496b81853c92cf04316e807100c554830fd96b2
Roger <roger.santos.main@gmail.com>
2018-09-11 13:51:14 -0500

    Added test case

--- _strings_tests.sh
+++ _strings_tests.sh
@@ -7,6 +7,7 @@ strings_tests::CountCharacters()
   strings::CountCharacters "1,2,3,4,5,6,7,8,9," "," | qa::AreEqual "9" "Could not count the characters properly"
   strings::CountCharacters ",,,,,,,,," "," | qa::AreEqual "9" "Could not count the characters properly"
   strings::CountCharacters "---------" "-" | qa::AreEqual "9" "Could not count the characters properly"
+  strings::CountCharacters 'm_szUnit, sizeof(m_szUnit)/sizeof(TCHAR), pszUnit != NULL ? pszUnit : _T(""), MAX_UNIT' "," | qa::AreEqual "3" "Could not count the characters properly"
 }
 
 
