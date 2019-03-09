ebcc1c18a643b17f14b19918b7b09e191356b761
Roger <roger.santos.main@gmail.com>
2019-03-09 08:21:56 -0600

    Fixed test to point to valid directory independent of the computer

--- _wsl_tests.sh
+++ _wsl_tests.sh
@@ -3,14 +3,19 @@
 pushd "$(dirname "$0")" > /dev/null
 
 source ./_qa.sh
+source ./_log.sh
 source ./_wsl.sh
 
 wsl_tests::ConvertLinuxPathToWindowsPath()
 {
-  wsl::ConvertLinuxPathToWindowsPath "/mnt/c/Users/roger.santos/work/PRODUCT/DEV/Builds/NT/OEM/Program Settings.ini" | qa::AreEqual "program_settings_path" "Could not convert Linux Path"
-  wsl::ConvertLinuxPathToWindowsPath "/c/Users/roger.santos/work/PRODUCT/DEV/Builds/NT/OEM/Program Settings.ini" | qa::AreEqual "program_settings_path" "Could not convert Linux Path"
+  wsl::ConvertLinuxPathToWindowsPath "/C/Program Files (x86)/Common Files" | qa::AreEqual "program_files_common" "Could not convert Linux Path"
+  wsl::ConvertLinuxPathToWindowsPath "/mnt/C/Program Files (x86)/Common Files" | qa::AreEqual "program_files_common" "Could not convert Linux Path"
+  wsl::ConvertLinuxPathToWindowsPath "/c/Windows/System32" | qa::AreEqual "system_32" "Could not convert Linux Path"
+  wsl::ConvertLinuxPathToWindowsPath "/mnt/c/Windows/System32" | qa::AreEqual "system_32" "Could not convert Linux Path"
 }
 
+# log::show_log "true"
+# log::level "5"
 qa::Init "builder"
 
 if [ "${1}" != "" ]; then
@@ -20,5 +25,6 @@ else
 fi
 
 qa::End
+# log::End
 
 popd > /dev/null
